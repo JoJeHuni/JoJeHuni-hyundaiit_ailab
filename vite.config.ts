2110b223218496197e4be4a6034910f37710fde7
JoJeHuni <cho9759@gmail.com>
2026-07-24 17:05:42 +0900

    vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    // GitHub Actions 배포 환경 감지하여 /hyundaiit_ailab/ 설정
-    base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : './',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
